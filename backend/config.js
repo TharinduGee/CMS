71a5499ea289be0ae602651f0cd453400acb6055
TharinduGee <tharindugeethanjan@gmail.com>
2023-09-26 11:54:04 +0530

    mongodb connection using mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = process.env.PORT || 3030;
 
-
+export const MongoDBUrl = 'mongodb+srv://tharindugeethanjan:30NazbIVIknRUuSO@cms.ywxuepg.mongodb.net/?retryWrites=true&w=majority';
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 import express from "express";
-import {PORT} from './config.js';
+import mongoose from "mongoose";
+import {PORT, MongoDBUrl} from './config.js';
 
 const app = express();
 
 app.get('/' , (req,res) => res.send('Welcome to CMS !!!'));
 
-app.listen(PORT, () => {
-     console.log(`Server is running on port ${PORT}`)
-});
 
- 
+
+mongoose
+     .connect(MongoDBUrl)
+     .then(() => {
+          console.log(`App is connected to database`);
+          app.listen(PORT, () => {
+               console.log(`Server is running on port ${PORT}`)
+          });
+     })
+     .catch((error) => {
+          console.log(error);
+     })
